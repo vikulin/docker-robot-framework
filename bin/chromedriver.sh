93b2eb37b32335dbd0160a6bdd44a3e633c3c7cd
Vadym Vikulin <vadym.vikulin@rivchain.org>
2024-01-31 01:49:27 +0200

    Run fix

--- bin/chromedriver.sh
+++ bin/chromedriver.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec /usr/local/bin/chromedriver --verbose --log-path=/var/log/chromedriver --no-sandbox --disable-gpu --disable-dev-shm-usage "$@"
+exec /usr/local/bin/chromedriver --verbose --log-path=/var/log/chromedriver --no-sandbox --remote-debugging-port=9222 --disable-gpu --disable-dev-shm-usage "$@"
