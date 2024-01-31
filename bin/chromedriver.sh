76c233e30e0f2c704298eaa2c95ee1b22a5a405f
Vadym Vikulin <vadym.vikulin@rivchain.org>
2024-01-31 14:55:03 +0200

    Code cleanup

--- bin/chromedriver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec /usr/bin/chromedriver --verbose --log-path=/var/log/chromedriver --no-sandbox --remote-debugging-port=9222 --disable-gpu --disable-dev-shm-usage "$@"
--- bin/google-chrome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec /usr/bin/google-chrome-original --headless=new --no-sandbox --disable-gpu --single-process --disable-dev-shm-usage --disable-dev-tools --no-zygote --remote-debugging-port=9222 "$@"
